cca8c8e56b32c58a02bd5511751e25dbbf07f7d4
Jason <asphyticz@outlook.com>
2024-06-02 13:26:42 +0800

    Initial Project Commit

---
+++ LibraryManagementSystem/JLMS/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using JLMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JLMS.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
